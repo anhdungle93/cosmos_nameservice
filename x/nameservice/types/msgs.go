4576d3abd22d7795c290bfbc20ee9f14004dc6a2
anhdungle <anhdungle634@gmail.com>
2020-06-05 16:55:16 +0200

    define MsgBuyName

--- x/nameservice/types/msgs.go
+++ x/nameservice/types/msgs.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 const RouterKey = ModuleName
@@ -26,7 +27,7 @@ func NewMsgSetName(name string, value string, owner sdk.AccAddress) MsgSetName {
 func (msg MsgSetName) Route() string { return RouterKey }
 
 // Type should return the action
-func (msg MsgSetName) Type() string { return "set_name"}
+func (msg MsgSetName) Type() string { return "set_name" }
 
 // ValidateBasic runs stateless checks on the message
 func (msg MsgSetName) ValidateBasic() error {
@@ -49,3 +50,48 @@ func (msg MsgSetName) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Owner}
 }
 
+// MsgBuyName defines the BuyName message
+type MsgBuyName struct {
+	Name  string         `json:"name"`
+	Bid   sdk.Coins      `json:"bid"`
+	Buyer sdk.AccAddress `json:"buyer"`
+}
+
+// NewMsgBuyName is the constructor function for MsgBuyName
+func NewMsgBuyName(name string, bid sdk.Coins, buyer sdk.AccAddress) MsgBuyName {
+	return MsgBuyName{
+		Name:  name,
+		Bid:   bid,
+		Buyer: buyer,
+	}
+}
+
+//Route should return the name of the module
+func (msg MsgBuyName) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgBuyName) Type() string { return "buy_name" }
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgBuyName) ValidateBasic() error {
+	if msg.Buyer.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Buyer.String())
+	}
+	if len(msg.Name) == 0 {
+		return sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Name cannot be empty")
+	}
+	if !msg.Bid.IsAllPositive() {
+		return sdkerrors.ErrInsufficientFunds
+	}
+	return nil
+}
+
+// GetSignBytes encodes the message for signing
+func (msg MsgBuyName) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+}
+
+// GetSigners defines whose signature is required
+func (msg MsgBuyName) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Buyer}
+}
