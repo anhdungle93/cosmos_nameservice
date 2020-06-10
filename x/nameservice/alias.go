9d5a42697708fbb8a81c232a12557872ff27c0a9
anhdungle <anhdungle634@gmail.com>
2020-06-10 11:46:31 +0200

    define aliases

--- x/nameservice/alias.go
+++ x/nameservice/alias.go
@@ -1,40 +1,43 @@
 package nameservice
 
 import (
-	"github.com/anhdungle/namespace/x/nameservice/keeper"
-	"github.com/anhdungle/namespace/x/nameservice/types"
+	"github.com/anhdungle93/namespace/x/nameservice/keeper"
+	"github.com/anhdungle93/namespace/x/nameservice/types"
 )
 
 const (
 	// TODO: define constants that you would like exposed from your module
 
-	ModuleName        = types.ModuleName
-	RouterKey         = types.RouterKey
-	StoreKey          = types.StoreKey
-	DefaultParamspace = types.DefaultParamspace
-	QueryParams       = types.QueryParams
-	QuerierRoute      = types.QuerierRoute
+	ModuleName   = types.ModuleName
+	RouterKey    = types.RouterKey
+	StoreKey     = types.StoreKey
+	QuerierRoute = types.QuerierRoute
 )
 
 var (
 	// functions aliases
-	NewKeeper           = keeper.NewKeeper
-	NewQuerier          = keeper.NewQuerier
-	RegisterCodec       = types.RegisterCodec
-	NewGenesisState     = types.NewGenesisState
-	DefaultGenesisState = types.DefaultGenesisState
-	ValidateGenesis     = types.ValidateGenesis
+	NewKeeper        = keeper.NewKeeper
+	NewQuerier       = keeper.NewQuerier
+	NewMsgBuyName    = types.NewMsgBuyName
+	NewMsgSetName    = types.NewMsgSetName
+	NewMsgDeleteName = types.NewMsgDeleteName
+	NewWhois         = types.NewWhois
+	RegisterCodec    = types.RegisterCodec
 	// TODO: Fill out function aliases
 
 	// variable aliases
-	ModuleCdc     = types.ModuleCdc
+	ModuleCdc = types.ModuleCdc
 	// TODO: Fill out variable aliases
 )
 
 type (
-	Keeper       = keeper.Keeper
-	GenesisState = types.GenesisState
-	Params       = types.Params
+	Keeper          = keeper.Keeper
+	MsgSetName      = types.MsgSetName
+	MsgBuyName      = types.MsgBuyName
+	MsgDeleteName   = types.MsgDeleteName
+	QueryResResolve = types.QueryResResolve
+	QueryResNames   = types.QueryResNames
+	Whois           = types.Whois
 
 	// TODO: Fill out module types
 )
